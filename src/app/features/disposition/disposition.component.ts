2e3106ff5bdeddbae9c51eb13e6b44e8b2f36c5e
Ramu Yedla <ramu.yedla@planonsoftware.com>
2020-07-13 17:01:49 +0530

    mocekd data for disposition page

--- src/app/features/disposition/disposition.component.ts
+++ src/app/features/disposition/disposition.component.ts
@@ -8,15 +8,42 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class DispositionComponent implements OnInit {
   compositionForm: FormGroup;
+  tableList: any;
+  selectedRow: any;
+  composedRows: any = [];
   constructor(private fromBuilder: FormBuilder) { }
   ngOnInit() {
     this.compositionForm = new FormGroup({
       type: new FormControl('', Validators.required)
     });
+    this.tableList = [{
+      client: '4 SC AG',
+      protocol: '4SC-205-1-2009',
+      facility: 'Aleton',
+      partId: '1609841'
+    },
+    {
+      client: '4 SC AG1',
+      protocol: '4SC-205-1-2009',
+      facility: 'Aleton',
+      partId: '1609842'
+    },
+    {
+      client: '4 SC AG2',
+      protocol: '4SC-205-1-2009',
+      facility: 'Aleton',
+      partId: '1609843'
+    }];
   }
 
 
   submit() {
-    console.log(this.compositionForm.value);
+    this.compositionForm.reset();
+    this.composedRows.push(this.selectedRow);
+    this.selectedRow = null;
+  }
+
+  selectRow(row) {
+    this.selectedRow = row;
   }
 }
